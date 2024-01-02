5b1c4db74614f60a54fa675664cb0b44c5a84cd6
Majd Khawaldeh <majd@gatech.edu>
2024-01-01 22:24:53 -0500

    allow post req

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [response, setResponse] = useState([]);
 
   const handleClick = () => {
-    fetch('http://127.0.0.1:5000/chat',{
+    fetch('http://mathbot.majdk.com/chat',{
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
